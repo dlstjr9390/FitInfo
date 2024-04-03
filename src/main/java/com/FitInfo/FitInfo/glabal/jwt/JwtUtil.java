31be4beef73bfd7259df376491029b08c6d7cd59
Yoon <dlstjr9390@gmail.com>
2024-04-04 03:49:25 +0900

    Jwt 유틸

--- src/main/java/com/FitInfo/FitInfo/glabal/jwt/JwtUtil.java
+++ src/main/java/com/FitInfo/FitInfo/glabal/jwt/JwtUtil.java
@@ -1,7 +1,21 @@
 package com.FitInfo.FitInfo.glabal.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -25,4 +39,60 @@ public class JwtUtil {
 
   private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
+  public static final Logger logger = LoggerFactory.getLogger("JWT 관련 로그");
+
+  @PostConstruct
+  public void init() {
+    byte[] bytes = Base64.getDecoder().decode();
+    key = Keys.hmacShaKeyFor(bytes);
+  }
+
+  public String createAccessToken(String username) {
+    Date date = new Date();
+
+    return Jwts.builder()
+        .setSubject(username)
+        .claim(AUTHORIZATION_KEY, "USER")
+        .setExpiration(new Date(date.getTime() + ACCESS_TOKEN_TIME))
+        .setIssuedAt(date)
+        .signWith(key, signatureAlgorithm)
+        .compact();
+  }
+
+  public void addJwtToCookie(String tokenName, String JWT, HttpServletResponse res) {
+
+    Cookie cookie = new Cookie(tokenName, JWT);
+    cookie.setPath("/");
+    cookie.setMaxAge(7 * 60 * 60 * 24); // 7일
+
+    if(tokenName.equals("refreshToken")) {
+      cookie.setHttpOnly(true);
+    }
+
+    res.addCookie(cookie);
+  }
+
+  public boolean validateToken(String token) throws ExpiredJwtException {
+    try {
+      Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+
+      return true;
+    } catch (SecurityException | MalformedJwtException | SignatureException e) {
+      logger.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+    } catch (ExpiredJwtException e) {
+      logger.error("Expired JWT token, 만료된 JWT token 입니다.");
+      throw e;
+    } catch (UnsupportedJwtException e) {
+      logger.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+    } catch (IllegalArgumentException e) {
+      logger.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+    }
+
+    return false;
+  }
+
+  public Claims getUserInfoFromToken(String token) {
+
+    return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+  }
 }
