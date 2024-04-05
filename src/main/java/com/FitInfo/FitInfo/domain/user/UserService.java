61bdfc1c46a78bb69a1e0c0707016b1c58bf9d9a
Yoon <dlstjr9390@gmail.com>
2024-04-05 23:00:40 +0900

    UserService 생성

---
+++ src/main/java/com/FitInfo/FitInfo/domain/user/UserService.java
@@ -0,0 +1,10 @@
+package com.FitInfo.FitInfo.domain.user;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+}
