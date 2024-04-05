76dc666c62bf21f3f8edf2bc3f0dda269bc0a067
Yoon <dlstjr9390@gmail.com>
2024-04-05 23:00:25 +0900

    UserController 회원가입 추가

--- src/main/java/com/FitInfo/FitInfo/domain/user/UserController.java
+++ src/main/java/com/FitInfo/FitInfo/domain/user/UserController.java
@@ -1,8 +1,23 @@
 package com.FitInfo.FitInfo.domain.user;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
 
+  private final UserService userService;
+
+  @PostMapping("/signup")
+  public String signupUser(@RequestBody SignupRequestDto signupRequestDto) {
+
+    return "회원가입 완료";
+  }
 }
