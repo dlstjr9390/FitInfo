d42c6862412d37214ef8b86b856f6b403b911924
Yoon <dlstjr9390@gmail.com>
2024-04-08 22:51:41 +0900

    로그 추가

--- src/main/java/com/FitInfo/FitInfo/global/jwt/JwtUtil.java
+++ src/main/java/com/FitInfo/FitInfo/global/jwt/JwtUtil.java
@@ -17,11 +17,13 @@ import java.net.URLDecoder;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class JwtUtil {
 
